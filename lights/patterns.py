04ebe82fad68e4e4ecc7106ad7b9c66850444d2b
Tristan iMac <wylde.larue@gmail.com>
2018-08-06 14:27:11 -0700

    added more tests to smooth out red on raninbow

--- lights/patterns.py
+++ lights/patterns.py
@@ -76,13 +76,46 @@ class AdjustedRainbowCycle(Pattern):
         self.display_name = "Adjusted Rainbow Cycle"
 
     def get_color(self, index, time, **kwargs):
-        normalized_sum = (index/300.0 + time) % 1
-        normalized_sum = normalized_sum**2
+        normalized_sum = (index/300.0 + time- 0.1) % 1
+        normalized_sum = (normalized_sum)**1.5
         red, green, blue = colorsys.hsv_to_rgb(normalized_sum, 1, 1)
         return Color(255*red, 255*green, 255*blue)
+
+class AdjustedRainbow(Pattern):
+    def __init__(self):
+        self.id_name = "adjusted_rainbow"
+        self.display_name = "Adjusted Rainbow"
+
+    def get_color(self, index, time, **kwargs):
+        normalized_sum = (time- 0.1) % 1
+        normalized_sum = (normalized_sum)**1.5
+        red, green, blue = colorsys.hsv_to_rgb(normalized_sum, 1, 1)
+        return Color(255*red, 255*green, 255*blue)
+
+class RedTest(Pattern):
+    def __init__(self):
+        self.id_name = "red_test"
+        self.display_name = "Red Test"
+
+    def get_color(self, index, time, **kwargs):
+        if 0 <= index < 60:
+            normalized_sum = 0
+        elif 60 < index < 120:
+            normalized_sum = 0.01
+        elif 180 < index < 240:
+            normalized_sum = 0.02
+        elif 240 < index < 300:
+            normalized_sum = 1 - 0.01
+        # normalized_sum = (normalized_sum)**1.5
+        red, green, blue = colorsys.hsv_to_rgb(normalized_sum, 1, 1)
+        return Color(255*red, 255*green, 255*blue)
+
+
 
 pattern_library = {
     "rainbow": Rainbow(),
     "rainbow_cycle": RainbowCycle(),
-    "adjusted_rainbow_cycle": AdjustedRainbowCycle()
+    "adjusted_rainbow_cycle": AdjustedRainbowCycle(),
+    "adjusted_rainbow": AdjustedRainbow(),
+    "red_test": RedTest()
 }
